001ecef6c48d95bdf8232f8d6c1b1053b03a790f
NDICHU <ndichumuriithi@gmail.com>
2022-04-22 03:03:55 +0300

    New approach thresh hold reached

--- main.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener(`DOMContentLoaded`, ()=> {
-    let origin = document.getElementById('fromPlaces');
-    new google.maps.places.Autocomplete(origin);
-    let destination = document.getElementById(`toPlaces`);
-    new google.maps.places.Autocomplete(destination);
-})
